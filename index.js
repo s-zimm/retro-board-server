825d784fadbec00f029d643456086918ae9dfdc6
Jonathan Ray <jonathanray17@gmail.com>
2022-11-16 21:28:20 -0500

    getting server ready for serverside move

--- index.js
+++ index.js
@@ -11,59 +11,60 @@ wss.on('connection', async (ws) => {
 	console.log('connected!')
 	ws.id = id++;
 	lookup[ws.id] = ws;
-	ws.send(JSON.stringify({ message: 'connected', socketId: ws.id, retroIds: await get_retroIDs('VersionOne.Web') || []}));
+	ws.send(JSON.stringify({ message: 'connected', socketId: ws.id, retroIds: await get_retroIDs(INSTANCE_ID) || []}));
 	
 	ws.on('message', async function message(data) {
 		const parsed = JSON.parse(data);
 		const message = parsed.message;
+		const INSTANCE_ID = parsed.instanceId;
 		console.log('MESSAGE', message)
 		switch(message) {
 			case 'create_retro': {
 				const { member, retroFormat, retroTitle } = parsed;
-				const retroState = await create_retro('VersionOne.Web', member, JSON.parse(retroFormat), retroTitle);
+				const retroState = await create_retro(INSTANCE_ID, member, JSON.parse(retroFormat), retroTitle);
 				ws.send(JSON.stringify({message: 'retro_created', retroState}));
 				break;
 			}
 			case 'join_retro': {
 				const { member, retroId } = parsed;
-				const updatedRetroState = await add_member('VersionOne.Web', retroId, member);
+				const updatedRetroState = await add_member(INSTANCE_ID, retroId, member);
 				updatedRetroState.members.forEach(member => lookup[member.socketId].send(JSON.stringify({message: 'retro_updated', retroState: updatedRetroState})));
 				break;
 			}
 			case 'add_card': {
 				const { card, column, retroId } = parsed;
-				const updatedRetroState = await add_card('VersionOne.Web', retroId, card, column);
+				const updatedRetroState = await add_card(INSTANCE_ID, retroId, card, column);
 				updatedRetroState.members.forEach(member => lookup[member.socketId].send(JSON.stringify({message: 'retro_updated', retroState: updatedRetroState})));
 				break;
 			}
 			case 'remove_card': {
 				const { card, column, retroId } = parsed;
-				const updatedRetroState = await remove_card('VersionOne.Web', retroId, card, column);
+				const updatedRetroState = await remove_card(INSTANCE_ID, retroId, card, column);
 				updatedRetroState.members.forEach(member => lookup[member.socketId].send(JSON.stringify({message: 'retro_updated', retroState: updatedRetroState})));
 				break;
 			}
 			case 'moveto_phase2': {
 				const { retroId } = parsed;
-				const updatedRetroState = await moveto_phase2('VersionOne.Web', retroId);
+				const updatedRetroState = await moveto_phase2(INSTANCE_ID, retroId);
 				updatedRetroState.members.forEach(member => lookup[member.socketId].send(JSON.stringify({message: 'retro_updated', retroState: updatedRetroState})));
 				break;
 			}
 			case 'moveto_phase3': {
 				const { retroId } = parsed;
-				const updatedRetroState = await moveto_phase3('VersionOne.Web', retroId);
+				const updatedRetroState = await moveto_phase3(INSTANCE_ID, retroId);
 				updatedRetroState.members.forEach(member => lookup[member.socketId].send(JSON.stringify({message: 'retro_updated', retroState: updatedRetroState})));
 				break;
 			}
 			case 'update_item': {
 				const { retroId, item } = parsed;
-				const updatedRetroState = await update_item('VersionOne.Web', retroId, item);
+				const updatedRetroState = await update_item(INSTANCE_ID, retroId, item);
 				updatedRetroState.members.forEach(member => lookup[member.socketId].send(JSON.stringify({message: 'retro_updated', retroState: updatedRetroState})));
 				break;
 			}
 			case 'close_retro': {
 				const { retroId, agilityRetroUrl } = parsed;
-				const members = await close_retro('VersionOne.Web', retroId);
-				const openRetros = await get_retroIDs('VersionOne.Web') || []
+				const members = await close_retro(INSTANCE_ID, retroId);
+				const openRetros = await get_retroIDs(INSTANCE_ID) || []
 				members.forEach(member => lookup[member.socketId].send(JSON.stringify({message: 'retro_closed', agilityRetroUrl, retroIds: openRetros})));
 				break;
 			}
